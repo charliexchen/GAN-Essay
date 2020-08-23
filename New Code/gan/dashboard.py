51a5cfccc261bf75ff5746fd167e5074627bea45
moretanks <c-chen@hotmail.co.uk>
2020-08-23 03:36:25 +0100

    Added kernel estimation for distribution, wired up with GAN

--- New Code/gan/dashboard.py
+++ New Code/gan/dashboard.py
@@ -1,17 +1,20 @@
 import pygame
 import numpy as np
 import math
+from colours import *
+from gan import GAN
 import random
+from collections import deque
 
 
 def get_screen_dims():
     display = pygame.display.get_surface()
     width = display.get_width()
     height = display.get_height()
-    return (width, height)
+    return width, height
 
 
-class histogram:
+class Histogram:
     """Function to create realtime histogram and functions"""
 
     def __init__(self, bucket_count: int, range_min: float, range_max: float):
@@ -39,60 +42,117 @@ class histogram:
     def scaled_histogram(self, scale: float = 1):
         return [scale * x / (self.bucket_size * self.count) for x in self.buckets]
 
-    def draw_histogram(self, screen, colour):
+    def draw_histogram(self, target_screen, colour):
         width, height = get_screen_dims()
         for ind, bucket in enumerate(self.scaled_histogram()):
             rectangle_height = bucket * height
             rectangle_width = width / self.bucket_count
             rect = pygame.Rect(ind * rectangle_width
                                , height, rectangle_width, -rectangle_height)
-            pygame.draw.rect(screen, colour, rect)
+            pygame.draw.rect(target_screen, colour, rect)
 
 
-class graph:
-    "function to creat realtime training graph"
+class Graph:
+    """function to creat realtime training graph"""
 
     def __init__(self, bucket_count: int, range_min: float, range_max: float):
         self.func_inputs = np.linspace(range_min, range_max, bucket_count)
         self.bucket_count = bucket_count
 
-    def draw_function(self, screen, colour, func, scale=1):
+    def draw_function(self, target_screen, colour, func, scale=1.0):
         width, height = get_screen_dims()
-        func_outputs = [height - int(func(x) * scale) for x in self.func_inputs]
+        func_outputs = [height - int(func(x) * scale * height) for x in self.func_inputs]
 
         drawing_points = list(zip(
             np.linspace(0, width, self.bucket_count),
             func_outputs
         ))
-        print(drawing_points)
-        pygame.draw.lines(screen, colour, False, drawing_points)
+        pygame.draw.lines(target_screen, colour, False, drawing_points)
+
+    def draw_vectorised_function(self, target_screen, colour, func, scale=1.0):
+        width, height = get_screen_dims()
+        func_outputs = func(self.func_inputs)
+
+        scaled_outputs = [height - int(x * scale * height) for x in func_outputs]
+
+        drawing_points = list(zip(
+            np.linspace(0, width, self.bucket_count),
+            scaled_outputs
+        ))
+        pygame.draw.lines(target_screen, colour, False, drawing_points)
 
 
-def f(x):
-    return x
+class GaussianKernel:
+    """Function for smoothing input function"""
+
+    def __init__(self, max_len=None):
+        self.datapoints = deque([], max_len)
+
+    def add_point(self, point):
+        self.datapoints.append(point)
+
+    def add_points(self, points):
+        self.datapoints.extend(points)
+
+    def evaluate(self, x, bandwidth=None):
+
+        n = len(self.datapoints)
+        if bandwidth is None:
+            if n < 5:
+                bandwidth = 1.0
+            else:
+                # Create rule of thumb bandwidth if non is assigned
+                sigma = np.var(list(self.datapoints))
+                bandwidth = sigma * ((4 / (3 * n)) ** 0.2)
+        return sum(GaussianKernel.gaussian(x, y, bandwidth) for y in list(self.datapoints)) / n
+
+    @staticmethod
+    def gaussian(x, mu=0.0, sigma=1.0):
+        return math.exp(-(((x - mu) / sigma) ** 2) / 2) / (math.sqrt(2 * math.pi) * sigma)
 
 
 if __name__ == "__main__":
 
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
+    gan = GAN()
 
-    boundary = [200, 200]
+
+    def eval_disc(x):
+        y = gan.discriminator.predict(x)
+        return y.flatten()
+
+
+    est = GaussianKernel(1024)
+
+
+    def optimal_disc(x):
+        p_fake = est.evaluate(x)
+        p_real = GaussianKernel.gaussian(x)
+        return p_real / (p_fake + p_real)
+
+
+    boundary = [500, 200]
     pygame.init()
     screen = pygame.display.set_mode(boundary)
     clock = pygame.time.Clock()
     done = False
-    hist = histogram(20, -2, 2)
-    gra = graph(20, -2, 2)
+    hist = Histogram(100, -6, 6)
+    gra = Graph(50, -6, 6)
+
+    count = 0
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  # If user clicked close
                 done = True
-        for i in range(100):
-            hist.add_datapoint(random.normalvariate(0, 1))
         screen.fill(BLACK)
+        samples = gan.train(++count).flatten()
+
+        hist.reset()
+        est.add_points(samples)
+        hist.add_datapoints(samples)
         hist.draw_histogram(screen, WHITE)
-        gra.draw_function(screen, RED, f, 100)
+        gra.draw_function(screen, RED, GaussianKernel.gaussian)
+
+        gra.draw_function(screen, GREEN, optimal_disc)
+        gra.draw_vectorised_function(screen, BLUE, eval_disc)
         pygame.display.flip()
         clock.tick(30)
--- New Code/gan/gan.py
+++ New Code/gan/gan.py
@@ -5,6 +5,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Reshape
 from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D
 from keras.layers import LeakyReLU, Dropout, BatchNormalization
+
+from keras import backend as K
 from keras.optimizers import Adam
 import numpy as np
 
@@ -26,7 +28,7 @@ class GAN(object):
     def exp(self, t):
         return 0.5 * np.exp(-t / 2)
 
-    def Norm(self, t, mu=7, s=0.8):
+    def Norm(self, t, mu=0, s=1):
         return (1 / (np.sqrt(np.pi * 2) * s)) * np.exp(-(t - mu) ** 2 / (2 * s * s))
 
     def bimodal(self, t):
@@ -35,10 +37,10 @@ class GAN(object):
     def generateData(self, batch, dist="Norm"):
         # This function samples from various true distributions, in order to create training data
         if dist == "Norm":
-            d = np.random.normal(loc=7, scale=0.8, size=[batch, 32])
+            d = np.random.normal(loc=0, scale=1.0, size=[batch, 1])
             return d
         elif dist == "Exp":
-            d = np.random.exponential(scale=2, size=[batch, 32])
+            d = np.random.exponential(scale=2, size=[batch, 1])
             return d
         elif dist == "Bimodal":
             d1 = np.random.normal(loc=8, scale=0.5, size=int(32 * batch / 2))
@@ -50,89 +52,44 @@ class GAN(object):
 
     def generateFakeData(self, batch_size):
         # This function samples from the generated distribution
-        noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 64])
+        noise = 10 * np.random.uniform(-1.0, 1.0, size=[batch_size, 1])
         return self.generator.predict([noise])
 
-    def train(self, train_steps=2000, batch_size=1024, k=5, k2=5, plottime=250):
+    def train(self, generation, batch_size=1024):
         # This is effectively algorithm 1 in the essay, with the heuristic game instead of the minimax game
-        for i in range(train_steps):
-            # This is the training loop for the discriminator
-            for j in range(k):
-                # sample from true distribution
-                data_train = self.generateData(batch_size, "Exp")
-                # sample from fake distribution
-                data_fake = self.generateFakeData(batch_size)
 
-                # Create training data
-                x = np.concatenate((data_train, data_fake))
-                y = np.ones([2 * batch_size, 1])
-                y[batch_size:, :] = 0
+        # This is the training loop for the discriminator
+        # sample from true distribution
+        data_train = self.generateData(batch_size, "Norm")
+        # sample from fake distribution
+        data_fake = self.generateFakeData(batch_size)
 
-                # Train the Discriminator
-                d_loss = self.discriminator.train_on_batch(x, y)
+        # Create training data
+        x = np.concatenate((data_train, data_fake))
+        y = np.ones([2 * batch_size, 1])
+        y[batch_size:, :] = 0
 
-            for j in range(k2):
-                # Choose labels to be 1.
-                # Plugging this into the cross entropy mean the objective for the generator maximises Log(D(G(x)))
-                # This is the heuristic game which gives us better gradients early in training
+        # Train the Discriminator
+        d_loss = self.discriminator.train_on_batch(x, y)
 
-                y = np.ones([batch_size, 1])
-                noise = np.random.uniform(-1.0, 1.0, size=[batch_size, 64])
-                a_loss = self.adversarial.train_on_batch(noise, y)
+        # Choose labels to be 1.
+        # Plugging this into the cross entropy mean the objective for the generator maximises Log(D(G(x)))
+        # This is the heuristic game which gives us better gradients early in training
 
-            # Print log message
-            log_mesg = "%d: [D loss: %f]" % (i, d_loss[0])
-            log_mesg = "%s  [A loss: %f]" % (log_mesg, a_loss[0])
+        y = np.ones([batch_size, 1])
+        noise = 10 * np.random.uniform(-1.0, 1.0, size=[batch_size, 1])
+        a_loss = self.adversarial.train_on_batch(noise, y)
 
-            # Print mean, variance to assess performance
-            dat = self.generateFakeData(100)
-            print(log_mesg + " [Mean: {}, S.D: {}]".format(np.mean(dat), np.std(dat)), end='\r')
-
-            # Periodically prints histogram to record progress
-            if i % plottime == 0:
-                self.plothist(1000, func=self.exp,
-                              title="GAN Learning Exponential Distribution with scale 2 - {} training steps".format(i))
-
-    def plothist(self, size, func, fake=True, title="Histogram with 'auto' bins"):
-        # Generate historgram by sampling from either the true or generated distribution
-        # This allows for performance to be assessed
-
-        # plots real distribution
-        x = np.arange(0., 12., 0.01)
-        y = [func(i) for i in x]
-
-        if fake:
-            # Plots histrogram with data generated from generator
-            plt.plot(x, y)
-            plt.hist(self.generateFakeData(size).flatten(), bins='auto',
-                     density=True)  # arguments are passed to np.histogram
-            plt.title(title)
-            plt.show()
-        else:
-            # Plots histrogram with data generated true distribution
-            plt.hist(np.asarray(self.generateData(size)).flatten(), bins='auto',
-                     density=True)  # arguments are passed to np.histogram
-            plt.title(title)
-            plt.show()
+        return data_fake
 
     def init_discriminator(self):
         # Defines the discriminator -- This is a densenet with sigmoid activations
         # The prior is a random vector
         self.D = Sequential()
-        dropout = 0.4
         # input is 32 samples from either a true or fake distribution
-        self.D.add(Dense(32, input_dim=32))
-        self.D.add(Activation('sigmoid'))
-        self.D.add(Dropout(dropout))
-
+        self.D.add(Dense(128, input_dim=1))
+        self.D.add(LeakyReLU(0.2))
         self.D.add(Dense(32))
-        self.D.add(Activation('sigmoid'))
-        self.D.add(Dropout(dropout))
-
-        self.D.add(Dense(32))
-        self.D.add(Activation('sigmoid'))
-        self.D.add(Dropout(dropout))
-
         self.D.add(Dense(1))
         # one final sigmoind to ensure that the output is a probability
         self.D.add(Activation('sigmoid'))
@@ -141,26 +98,22 @@ class GAN(object):
 
     def bigtanh(self, x):
         # We use this scaled tanh as the output rule to shift the range to [0,12] from [0,1]
-        return (K.tanh(x) * 6 + 6)
+        return (K.tanh(x) * 6)
 
     def init_generator(self):
         # Defines the discriminator -- attempts to generate a vector of uniformly distributed distributions
         # This is a densenet with ReLU activations
         self.G = Sequential()
-        dropout = 0.4
 
-        self.G.add(Dense(64, input_dim=64))
-        self.G.add(LeakyReLU(alpha=0.2))
-        self.G.add(Dropout(dropout))
+        self.G.add(Dense(256, input_dim=1))
+        self.G.add(LeakyReLU(0.2))
 
-        self.G.add(Dense(32))
-        self.D.add(LeakyReLU(alpha=0.2))
-        self.G.add(Dropout(dropout))
+        self.G.add(Dense(8))
+        self.G.add(LeakyReLU(0.2))
 
-        self.G.add(Dense(32))
+        self.G.add(Dense(1))
         # Custom output to ensure that that the output is on the correct domain
         self.G.add(Activation(self.bigtanh))
-        self.G.add(Dropout(dropout))
 
         self.G.summary()
         return self.G
@@ -178,7 +131,7 @@ class GAN(object):
     def adversarial_model(self):
         # Defines the objective for the generator
         # We use the adam gradient method for both networks
-        optimizer = Adam(lr=0.0003, decay=10e-8)
+        optimizer = Adam(lr=0.003, decay=10e-8)
         self.AM = Sequential()
         self.AM.add(self.init_generator())
         self.AM.add(self.init_discriminator())
@@ -186,4 +139,3 @@ class GAN(object):
         self.AM.compile(loss='binary_crossentropy', optimizer=optimizer, \
                         metrics=['accuracy'])
         return self.AM
-
